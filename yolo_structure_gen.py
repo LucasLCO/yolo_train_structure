0dc91724b12300be6b621f2495800a46fea81d27
LucasLCO <lucaslco2005@gmail.com>
2023-12-04 13:39:55 -0300

    feat: add test path option to yaml gen

--- yolo_structure_gen.py
+++ yolo_structure_gen.py
@@ -6,21 +6,24 @@ def gen_names_string(names_dict):
         string += f"  {key}: {names_dict[key]}\n"
     return string
 
-def gen_yaml_info(train_images_path, val_images_path, names_dict):
-    data_info = f"train: {str(train_images_path.absolute())}\n"
-    data_info += f"val: {str(val_images_path.absolute())}\n"
+def gen_yaml_info(images_path, names_dict, test):
+    data_info = f"train: {str(images_path.absolute())}/train\n"
+    data_info += f"val: {str(images_path.absolute())}/val\n"
+    if test:
+        data_info += f"val: {str(images_path.absolute())}/test\n"
     data_info += f"names:\n"
     data_info += f"{gen_names_string(names_dict)}"
     return data_info
 
 def gen_paths(names_dict, test):
+    images_path = Path("./dataset/images")
     train_images_path = Path("./dataset/images/train")
     train_labels_path = Path("./dataset/labels/train")
     val_images_path = Path("./dataset/images/val")
     val_labels_path = Path("./dataset/labels/val")
     data_yaml_path = Path("./dataset/data.yaml")
-    data_info = gen_yaml_info(train_images_path, val_images_path, names_dict)
-    
+    data_info = gen_yaml_info(images_path, names_dict, test)
+
     if not test:
         return (train_images_path, train_labels_path, val_images_path,
                 val_labels_path, data_yaml_path, data_info)
